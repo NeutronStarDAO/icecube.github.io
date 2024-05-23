16e62c3134ddbbdbb9407bb07d3b604344615872
NeutronStarPRO <nashzhengpro@gmail.com>
2024-05-23 15:45:06 +0800

    Add files via upload

--- technology/script.js
+++ technology/script.js
@@ -151,22 +151,6 @@ function initBlobs() {
 initBlobs();
 
 
-
-const TOGGLE = document.querySelector("button");
-
-const UPDATE = () => {
-  const DARK = TOGGLE.matches("[aria-pressed=true]");
-  TOGGLE.setAttribute("aria-pressed", DARK ? false : true);
-  document.documentElement.className = DARK ? "dark" : "";
-};
-
-const TOGGLE_THEME = () => {
-  if (!document.startViewTransition) return UPDATE();
-  document.startViewTransition(() => UPDATE());
-};
-
-TOGGLE.addEventListener("click", TOGGLE_THEME);
-
 if (!CSS.supports("animation-timeline: view()")) {
   const MARKS = document.querySelectorAll("mark");
   const OPTS = {
